3e45aee3d58c071da09449d07ace7a24c3986dc5
Sandro Pischinger <sandro@psandro.eu>
2021-03-26 20:31:18 +0100

    :alembic: more experiments with queue

--- mimpf/queue.py
+++ mimpf/queue.py
@@ -5,13 +5,12 @@ def waiting_time_priority(waiting_time_diff: int):
     """
     if waiting_time_diff < 0:
         raise ValueError("waiting time cannot be negative")
-        return 1
 
     elif waiting_time_diff < 15:
-        return (-0.25/15) * waiting_time_diff + 1
+        return (-0.25 / 15) * waiting_time_diff + 1
 
     elif waiting_time_diff < 15:
-        return (-0.75/30) * waiting_time_diff + 0.75
+        return (-0.75 / 30) * waiting_time_diff + 0.75
 
     else:
         return 0
@@ -25,21 +24,34 @@ def target_date_priority(target_date_diff: int):
     if target_date_diff < -25:
         return 1
     elif target_date_diff < -5:
-        return (-1/25) * target_date_diff + 1
+        return (-1 / 20) * target_date_diff + 1
     elif target_date_diff < 15:
         return 0
-    elif target_date_diff < 30:
-        return (0.5/15) * target_date_diff
     else:
         return 0.5
 
 
-def calculate_priority(waiting_time_diff: int, target_date_diff: int):
+def punctuality_priority(arrival_date_diff: int):
+    """
+    calculate the priority (0 to 1, lower = better) according
+    to the punctuality (target date minus arrival date) in minutes.
+    """
+    if arrival_date_diff < -15:
+        return 1
+    elif arrival_date_diff < -5:
+        return (-1 / 10) * arrival_date_diff + 1
+    elif arrival_date_diff < 5:
+        return 0
+    elif arrival_date_diff < 10:
+        return (1 / 5) * arrival_date_diff
+    else:
+        return 1
+
+
+def calculate_priority(waiting_time_diff: int, target_date_diff: int, arrival_date_diff: int):
     """
     calculate the priority of an appointment based on
-    waiting time (minutes), difference to target date (minutes).
-
+    waiting time (minutes), difference to target date (minutes), punctuality (minutes).
     """
-    return target_date_priority(target_date_diff)
-    * waiting_time_priority(waiting_time_diff)
-
+    return (target_date_priority(target_date_diff) + waiting_time_priority(waiting_time_diff) \
+            + punctuality_priority(arrival_date_diff)) / 3
