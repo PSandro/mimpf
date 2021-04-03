2db421dd5cee8541ee42e2a9770c855b7bc5684a
Sandro Pischinger <sandro@psandro.eu>
2021-04-03 21:50:38 +0200

    :wastebasket: clean

--- mimpf/static/assets/js/mimpf-api.js
+++
@@ -1,6 +0,0 @@
-const dcrf = require('dcrf-client');
-const client = dcrf.connect('ws://127.0.0.1')
-
-client.retrieve('appointment', 1).then(appointment => {
-  console.info('Retrieved appointment 1:', appointment);
-});
--- mimpf/tasks.py
+++
@@ -1,7 +0,0 @@
-from .models import QueueEntry
-
-
-def update_pending_priorities():
-
-    for query_entry in QueueEntry.objects.all():
-        priority = query_entry.get_priority()
