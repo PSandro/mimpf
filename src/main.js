34d10ed7974af9dbbc644c8d6687040b80ab65dd
Sandro Pischinger <sandro@psandro.eu>
2021-04-04 11:43:42 +0200

    socket.io, vue3, express init

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import VueSocketIO from 'vue-socket.io'
+
+
+const optionsVueIO = {
+	debug: true,
+	connection: "http://localhost:3000",
+}
+
+const app = createApp(App).use(new VueSocketIO(optionsVueIO));
+app.mount('#app');
